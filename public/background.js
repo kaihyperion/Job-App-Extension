f6b1eee2718df731eeb81f8df3e7b47cb7fc7ed4
kaihyperion <kyperion.workmode@gmail.com>
2024-10-02 10:56:14 -0700

    added comments on app.js

--- public/background.js
+++ public/background.js
@@ -4,3 +4,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         sendResponse({ status: "Job application started"});
     }
 });
+
+//background service worker (background script) is responsible for handling events and logic that need to run in the background, independently of the browser's tabs or user interaction
+/*background service worker is event-driven. it waits for specific events (message or browser action) and reacts to them
+in Manifest V3, background scripts have been replaced by service workers (efficient and needs less memory consumption) */
--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
+/*global chrome*/
 import React, { useState } from 'react';
 
 function App() {
+  // state declaration
+   // const [state, setState] = useState(initialValue);
   const [jobUrl, setJobUrl] = useState('');
   const [resumeType, setResumeType] = useState('swe');
   const [status, setStatus] = useState('');
 
+
+  // handleApply function is triggered when user clicks 'apply' button
   const handleApply = () => {
-    chrome.runtime.sendMessage({
-      action: "apply",
-      jobUrl: jobUrl,
-      resumeType: resumeType
-    }, (response) => {
-      setStatus(response.status);
-    });
+    // Check if the 'chrome' API is available beczu without chrome, it wshould't work
+    if (typeof chrome !== 'undefined' && chrome.runtime) {
+      // Call the Chrome API to send a message to the background script
+      chrome.runtime.sendMessage(
+        {
+          action: "apply",
+          jobUrl: jobUrl,
+          resumeType: resumeType
+        },
+        (response) => {
+          setStatus(response.status);
+        }
+      );
+    } else {
+      // if not chrome API, send message to consloe
+      console.log("Chrome API not available. This code only works inside a Chrome extension.");
+      setStatus("Chrome API not available.");
+    }
   };
 
+  // our app(extension) will return the js rendering
   return (
     <div style={{ padding: '10px' }}>
       <h2>Job Application Automator</h2>
@@ -33,8 +50,12 @@ function App() {
       >
         <option value="swe">Software Engineer</option>
         <option value="mle">Machine Learning Engineer</option>
+        <option value="ds">Data Scientist</option>
+        <option value="analyst">Analyst</option>
+        <option value="app">App Developer</option>
       </select><br /><br />
-      <button onClick={handleApply}>Apply</button>
+      {/* on the botton click, it will call the handleApply function */}
+      <button onClick={handleApply}>Apply</button> 
       {status && <p>{status}</p>}
     </div>
   );
